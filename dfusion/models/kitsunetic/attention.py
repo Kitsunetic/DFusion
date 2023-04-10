323824d42f331215b51a94f507e933fbfa8573ea
Kitsunetic <jh.shim.gg@gmail.com>
2023-04-10 17:51:04 +0900

    fix cross attention error

--- dfusion/models/kitsunetic/attention.py
+++ dfusion/models/kitsunetic/attention.py
@@ -118,8 +118,8 @@ def zero_module(module):
     return module
 
 
-def Normalize(in_channels):
-    return torch.nn.GroupNorm(num_groups=32, num_channels=in_channels, eps=1e-6, affine=True)
+def Normalize(in_channels, num_groups=32):
+    return torch.nn.GroupNorm(num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True)
 
 
 def count_flops_attn(model, _x, y):
