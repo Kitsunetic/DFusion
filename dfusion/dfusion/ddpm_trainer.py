964a89edad746690213029e06c866586a0724d75
Kitsunetic <jh.shim.gg@gmail.com>
2023-07-17 20:56:50 +0900

    add `return_info` option for DDPMTrainer

--- dfusion/dfusion/ddpm_trainer.py
+++ dfusion/dfusion/ddpm_trainer.py
@@ -54,38 +54,48 @@ class DDPMTrainer(DiffusionBase):
         decoder_nll = -discretized_gaussian_log_likelihood(x_start, model_mean, 0.5 * model_log_var)
         decoder_nll = decoder_nll.flatten(1).mean(1)
 
-        vlb = th.where(t == 0, decoder_nll, kl)
-        return vlb
+        out["vlb"] = th.where(t == 0, decoder_nll, kl)
+        return out
 
-    def forward(self, denoise_fn: Callable[[Tensor, Tensor], Tensor], x_start: Tensor, t: Tensor = None, noise: Tensor = None):
+    def forward(
+        self,
+        denoise_fn: Callable[[Tensor, Tensor], Tensor],
+        x_start: Tensor,
+        t: Tensor = None,
+        noise: Tensor = None,
+        return_info=False,
+    ):
         b = x_start.size(0)
         t = default(t, lambda: th.randint(0, self.num_timesteps, (b,), dtype=th.long, device=self.device))
         noise = default(noise, lambda: th.randn_like(x_start))
         x_t = self.q_sample(x_start, t, noise)
 
         losses = {}
+        info = {"t": t, "eps": noise, "x_t": x_t, "x_start": x_start}
         if self.loss_type in ("kl", "rescaled_kl"):
             # \mathcal L_\text{kl}
-            losses["loss"] = self.get_vlb(denoise_fn, x_start, x_t, t)
+            info.update(self.get_vlb(denoise_fn, x_start, x_t, t))
+            losses["loss"] = info["vlb"]
 
         elif self.loss_type in ("l2", "rescaled_l2", "l1", "rescaled_l1"):
             # \mathcal L_\text{vlb}
             model_out = denoise_fn(x_t, t)
             if self.model_var_type in ("learned", "learned_range"):
-                assert model_out.size(1) == 2 * x_t.size(1)
+                assert model_out.size(1) == 2 * x_t.size(1), "output channel must be double of input channel for learned_*"
                 model_out, tmp = th.chunk(model_out, chunks=2, dim=1)
                 frozen_out = th.cat([model_out.detach(), tmp], dim=1)
-                losses["vlb"] = self.get_vlb(lambda *_: frozen_out, x_start, x_t, t)
+                info.update(self.get_vlb(lambda *_: frozen_out, x_start, x_t, t))
+                losses["vlb"] = info["vlb"]
                 if self.loss_type in ("rescaled_l1", "rescaled_l2"):
                     losses["vlb"] = losses["vlb"] * self.num_timesteps / 1000.0
             # note: vlb is not calculated when its fixed variance setting, but the fixed variances are used during sampling
 
             # \mathcal L_\text{simple}
-            target = {
-                "eps": lambda: noise,
-                "x_start": lambda: x_start,
-                "x_prev": lambda: self.q_posterior_mean_variance(x_start, x_t, t)[0],
-            }[self.model_mean_type]()
+            x_prev = self.q_posterior_mean_variance(x_start, x_t, t)[0]
+            info["x_prev"] = x_prev
+
+            target = {"eps": noise, "x_start": x_start, "x_prev": x_prev}[self.model_mean_type]
+            info["target"] = target
             losses["recon"] = self.loss_fn(model_out, target)
 
             # \mathcal L = \mathcal L_\text{simple} * \lambda_\text{p2}
@@ -98,7 +108,7 @@ class DDPMTrainer(DiffusionBase):
             if "vlb" in losses:
                 losses["loss"] = losses["loss"] + losses["vlb"]
 
-        return losses
+        return (losses, info) if return_info else losses
 
 
 def __test__():
@@ -106,9 +116,24 @@ def __test__():
     diffusion_trainer = DDPMTrainer(betas, model_var_type="learned_range", p2_loss_weight_gamma=1.0)
     model = lambda x, t: th.cat([x, x], dim=1)  # any model that doubles the channel (only when learnt model variance)
     data = th.rand(2, 3, 4, 5, 6, 7, 8, 9)  # arbitrary shape
-    losses = diffusion_trainer(model, data)
+    losses, info = diffusion_trainer(model, data, return_info=True)
     print(losses)
     # {'vlb': tensor([5.2729e-06, 5.2618e-05]), 'eps': tensor([0.0007, 0.0084]), 'loss': tensor([0.0007, 0.0082])}
+
+    for k, v in info.items():
+        print(k, ":", v.shape if isinstance(v, th.Tensor) else v)
+    # t : torch.Size([2])
+    # eps : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # x_t : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # x_start : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # model_out : torch.Size([2, 6, 4, 5, 6, 7, 8, 9])
+    # model_log_var : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # pred_eps : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # model_mean : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # pred_x_start : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # vlb : torch.Size([2])
+    # x_prev : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
+    # target : torch.Size([2, 3, 4, 5, 6, 7, 8, 9])
 
 
 if __name__ == "__main__":
